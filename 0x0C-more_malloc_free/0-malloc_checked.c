9238d2ecaa34e17aea993a0aa765d0be1286fe46
Susanbudu <you@example.comcd>
2023-09-22 06:54:26 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,38 +1,17 @@
 #include "main.h"
-#include <stdlib.h>
-
 /**
- * create_array - Creates an array of chars and
- *                initializes it with a specific char.
- * @size: The size of the array to be initialized.
- * @c: The specific char to intialize the array with.
- *
- * Return: If size == 0 or the function fails - NULL.
- *         Otherwise - a pointer to the array.
- */
-char *create_array(unsigned int size, char c)
+ * malloc_checked - pointer without data type
+ * @b: value
+ * Return:  pointer
+ **/
+void *malloc_checked(unsigned int b)
 {
-	char *pointer1;
-	unsigned int i;
+	void *pointer;
 
-	if (size == 0)
-	{
-		pointer1 = NULL;
-		return (pointer1);
-	}
-	else
-	{
-		pointer1 = (char *)malloc(size * sizeof(*pointer1));
-		if (pointer1 != NULL)
-		{
-			for (i = 0; i < size; i++)
-				*(pointer1 + i) = c;
-	}
-		else
-		{
-			pointer1 = NULL;
-			return (pointer1);
-		}
-	}
-	return (pointer1);
+	pointer = malloc(b);
+	if (pointer != NULL)
+		return (pointer);
+	if (pointer == NULL)
+		exit(98);
+	exit(98);
 }
