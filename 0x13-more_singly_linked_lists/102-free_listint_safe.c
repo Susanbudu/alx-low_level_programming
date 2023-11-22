942eff47bc5f939b1b36c5821ef425ee03e3eae9
Susanbudu <budususan2@gmail.com>
2023-11-21 23:26:00 -0800

    work

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees a listint_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: the size of the list that was freed
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	size_t size = 0;
+	listint_t *current, *next;
+
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	current = *h;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		size++;
+		if (next >= current)
+			break;
+		current = next;
+	}
+	*h = NULL;
+	return (size);
+}
